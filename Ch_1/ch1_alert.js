3acc186b92b2339713d0a466b959bc9ea4d36f76
Elliotte <ciaontop>
2024-10-31 22:21:44 -0400

    Initial commit for JavaScript book repository

---
+++ Ch_1/ch1_alert.js
@@ -0,0 +1,6 @@
+prompt("Hi! How are you?");
+
+let a = 10; // value set at 10
+console.log(a);
+/**This is a multiline comment */
+
---
+++ Ch_1/ch1_project.js
@@ -0,0 +1,4 @@
+alert(
+  "Hi! My Name Is Elliotte Wideman"
+);
+/**This is my name sending output to the console of my Ch1_project html file */
